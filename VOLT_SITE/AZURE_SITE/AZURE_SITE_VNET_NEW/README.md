337fd5a46e5302c5e09edd4af2383589b18f42fe
MattDierick <matt262810@gmail.com>
2021-10-25 13:09:51 +0200

    new vnet

---
+++ VOLT_SITE/AZURE_SITE/AZURE_SITE_VNET_NEW/README.md
@@ -0,0 +1,3 @@
+## Create an AZURE Site for new Vnet
+
+More content and documentation here :
---
+++ VOLT_SITE/AZURE_SITE/AZURE_SITE_VNET_NEW/variables.tf
@@ -0,0 +1,14 @@
+variable "api_cert" {
+            type = string
+            default = "/PATH/certificate.cert"
+        }
+        
+        variable "api_key" {
+          type = string
+          default = "/PATH/private_key.key"
+        }
+        
+        variable "api_url" {
+            type = string
+            default = "https://YOUR_TENANT.console.ves.volterra.io/api"
+        }
