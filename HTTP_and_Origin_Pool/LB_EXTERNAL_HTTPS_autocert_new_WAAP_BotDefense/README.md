28982dc7753f2442bef5ae0cbb7f413f7931bade
MattDierick <matt262810@gmail.com>
2022-09-23 16:46:47 +0200

    bot-defense

---
+++ HTTP_and_Origin_Pool/LB_EXTERNAL_HTTPS_autocert_new_WAAP_BotDefense/README.md
@@ -0,0 +1,5 @@
+## Create an Internet exposed HTTPS Load-Balancer with a WAAP Policy assigned on Volterra with Terraform (Origin handled by a Volterra node) + Bot Defense protection
+
+In this example, we create a new WAAP policy and we refer to this policy in the HTTP declaration
+What's more we will enable F5 Bot Defense
+More content and documentation here : https://devcentral.f5.com/s/articles/Create-an-Internet-exposed-HTTPS-Load-Balancer-on-Volterra-with-Terraform
---
+++ HTTP_and_Origin_Pool/LB_EXTERNAL_HTTPS_autocert_new_WAAP_BotDefense/volterra_op_lb-https_autocert_new_waap.tf
@@ -0,0 +1,123 @@
+//==========================================================================
+//Definition of the Origin, 1-origin.tf
+//Start of the TF file
+resource "volterra_origin_pool" "op-public-fqdn" {
+  name                   = "op-public-fqdn"
+  //Name of the namespace where the origin pool must be deployed
+  namespace              = "VOLTERRA_NS"
+ 
+   origin_servers {
+
+    public_name {
+      dns_name = "APP_FQDN"
+    }
+
+    labels = {
+    }
+  }
+
+  no_tls = true
+  port = "80"
+  endpoint_selection     = "LOCALPREFERED"
+  loadbalancer_algorithm = "LB_OVERRIDE"
+}
+//End of the file
+//==========================================================================
+
+//Definition of the WAAP Policy
+resource "volterra_app_firewall" "waap-tf" {
+  name      = "WAAP_POLICY_TO_CREATE"
+  namespace = "VOLTERRA_NS"
+
+  // One of the arguments from this list "allow_all_response_codes allowed_response_codes" must be set
+  allow_all_response_codes = true
+  // One of the arguments from this list "default_anonymization custom_anonymization disable_anonymization" must be set
+  default_anonymization = true
+  // One of the arguments from this list "use_default_blocking_page blocking_page" must be set
+  use_default_blocking_page = true
+  // One of the arguments from this list "default_bot_setting bot_protection_setting" must be set
+  default_bot_setting = true
+  // One of the arguments from this list "default_detection_settings detection_settings" must be set
+  default_detection_settings = true
+  // One of the arguments from this list "use_loadbalancer_setting blocking monitoring" must be set
+  use_loadbalancer_setting = true
+  // Blocking mode - optional - if not set, policy is in MONITORING
+  blocking = true
+}
+
+//==========================================================================
+//Definition of the Load-Balancer, 2-https-lb.tf
+//Start of the TF file
+resource "volterra_http_loadbalancer" "lb-https-tf" {
+  depends_on = [volterra_origin_pool.op-public-fqdn]
+  //Mandatory "Metadata"
+  name      = "lb-https-tf"
+  //Name of the namespace where the origin pool must be deployed
+  namespace = "VOLTERRA_NS"
+  //End of mandatory "Metadata" 
+  //Mandatory "Basic configuration" with Auto-Cert 
+  domains = ["mypublic.appfqdn.com"]
+  https_auto_cert {
+    add_hsts = true
+    http_redirect = true
+    no_mtls = true
+    enable_path_normalize = true
+    tls_config {
+        default_security = true
+      }
+  }
+  default_route_pools {
+      pool {
+        name = "op-public-fqdn"
+        namespace = "VOLTERRA_NS"
+      }
+      weight = 1
+    }
+  //Mandatory "VIP configuration"
+  advertise_on_public_default_vip = true
+  //End of mandatory "VIP configuration"
+  //Mandatory "Security configuration"
+  no_service_policies = true
+  no_challenge = true
+  disable_rate_limit = true
+  //WAAP Policy reference, created earlier in this plan - refer to the same name
+  app_firewall {
+    name = "WAAP_POLICY_TO_CREATE"
+    namespace = "VOLTERRA_NS"
+  }
+  multi_lb_app = true
+  user_id_client_ip = true
+  //End of mandatory "Security configuration"
+  //Mandatory "Load Balancing Control"
+  source_ip_stickiness = true
+  //End of mandatory "Load Balancing Control"
+  
+  //Bot Defense//
+  bot_defense {
+    policy {
+      js_insert_all_pages {
+        javascript_location = "AFTER_HEAD"
+      }
+      protected_app_endpoints {
+        http_methods = ["METHOD_POST"]
+        metadata {
+          name = "name-endpoint"
+        }
+        mitigation {
+          block {
+            status = "PaymentRequired"
+          }
+        }
+        path {
+          path = "/endpoint_url"
+        }
+      }
+    }
+    regional_endpoint = "EU"
+  }
+  //End of Bot Defense//
+
+}
+
+//End of the file
+//==========================================================================
